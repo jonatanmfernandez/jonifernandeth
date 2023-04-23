95cb2b67377c9709e1daccea5ba28878291c0ebb
jonifz <fernandezjonatan.m@gmail.com>
2023-04-23 11:38:04 -0300

    firebaseprueba

---
+++ assets/js/node.js
@@ -0,0 +1,8 @@
+var admin = require("firebase-admin");
+
+var serviceAccount = require("path/to/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://portfolio-de73a-default-rtdb.firebaseio.com"
+});
